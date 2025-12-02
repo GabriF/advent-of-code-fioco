f70d834c554a5374f5a38b9ba2084b6b8c566417
Gabriele Fioco <me@fioco.eu>
2025-12-02 15:05:54 +0100

    python 2025 new day 02.2

--- 2025/d02.py
+++ 2025/d02.py
@@ -1,18 +1,44 @@
-def is_repeated(n : int) -> bool:
+def partition(string : str, part_len : int) -> list[str]:
+    assert len(string) % part_len == 0
+    string_list : list[str] = []
+    for start in range(0, len(string), part_len): string_list.append(string[start:start+part_len])
+    return string_list
+
+def all_same(str_list : list[str]) -> bool:
+    checked_val = str_list[0]
+    for val in str_list[1:]:
+        if val != checked_val: return False
+    return True
+
+def get_factors(n : int) -> list[int]:
+    return [x for x in range(1, int(n / 2) + 1) if n % x == 0]
+
+def is_invalid(n : int, parts_len : list[int]) -> bool:
     n_str : str = str(n)
-    n_len_half : int = int(len(n_str) / 2)
-    return n_str[:n_len_half] == n_str[n_len_half:]
+    n_len = len(n_str)
+    for checked_len in parts_len:
+        if (n_len % checked_len != 0): continue
+        parts = partition(n_str, checked_len)
+        if all_same(parts): return True
+    return False
 
 def count_invalid(number_range : list[str]) -> int:
     tokens = number_range.split('-')
     n_1 = int(tokens[0])
     n_2 = int(tokens[1])
-    return sum(n for n in range(n_1, n_2 + 1) if is_repeated(n))
+    s_1 = sum(n for n in range(n_1, n_2 + 1) if is_invalid(n, [2]))
+    s_2 = sum(n for n in range(n_1, n_2 + 1) if is_invalid(n, get_factors(len(str(n)))))
+    return (s_1, s_2)
 
 file_name = '2025/input/d02.txt'
 
 with open(file_name, 'r') as file:
     line = file.readline().strip()
     tokens = line.split(',')
-    s = sum(count_invalid(number_range) for number_range in tokens)
-    print(s)
+    s_0 = 0
+    s_1 = 0
+    for number_range in tokens:
+        counters = count_invalid(number_range)
+        s_0 += counters[0]
+        s_1 += counters[1]
+    print((s_0, s_1))
