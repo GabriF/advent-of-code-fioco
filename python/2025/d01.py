5ba1628b1ef91a00d16bd3bed3002138fe5418ca
Gabriele Fioco <me@fioco.eu>
2025-12-03 11:14:54 +0100

    python 2025 fix day 01.1

--- python/2025/d01.py
+++ python/2025/d01.py
@@ -1,20 +1,18 @@
+import math
 import sys
 
 max_val = 100
 curr = 50
-res = 0
+s_1 = 0
 direction_mult = {'L': -1, 'R': 1}
 for line in sys.stdin.readlines():
     line = line.strip()
     direction = line[0]
     count = int(line[1:])
-    curr = (curr + count * direction_mult[direction])
 
-    if curr < 0:
-        curr = max_val + curr
-    elif curr > 99:
-        curr = curr % max_val
+    curr += (count * direction_mult[direction])
+    if curr < 0: curr = max_val + curr
+    curr = curr % max_val
 
-    if curr == 0:
-        res += 1
-print(res)
+    if curr == 0: s_1 += 1
+print(s_1)
